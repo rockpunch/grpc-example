406ff9330ab6d330d0432cfc4466dc59a5abdbc7
sehy0121 <sbae3@artic.edu>
2020-03-04 13:30:55 +0900

    generated greet.pb.go

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-protoc protoc -I greet/greetpb/ greet/greetpb/greet.proto --go_out=plugins=grpc:greet/greetpb
+protoc -I greet/greetpb greet/greetpb/greet.proto --go_out=plugins=grpc:greet/greetpb
